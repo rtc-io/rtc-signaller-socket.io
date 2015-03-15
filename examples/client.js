8296d62771903c4eaffa5e73bd72a4dd65fee112
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-15 20:03:41 +1100

    Added examples

---
+++ examples/client.js
@@ -0,0 +1,15 @@
+var quickconnect = require('rtc-quickconnect');
+var signaller = require('..')('http://localhost:3000');
+var freeice = require('freeice');
+var qc = quickconnect(signaller, {
+  room: 'socketio-signalling-demo',
+  iceServers: freeice()
+});
+
+// let quickconnect know we want a datachannel
+qc.createDataChannel('test');
+
+// once the data channel is opened, let us know
+qc.on('channel:opened:test', function(id, dc) {
+  console.log('data channel opened with peer: ' + id);
+});
---
+++ examples/server.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var server = http.createServer();
+var io = require('socket.io')(server);
+var board = require('rtc-switch')();
+var port = process.env.PORT || 3000;
+
+io.on('connection', function(socket){
+  var peer = board.connect();
+
+  socket.on('message', peer.process);
+  peer.on('data', function(data) {
+    socket.send(data);
+  });
+});
+
+server.listen(port, function(err) {
+  if (err) {
+    return console.error('could not start server: ', err);
+  }
+
+  console.log('server running @ http://localhost:' + port);
+});
