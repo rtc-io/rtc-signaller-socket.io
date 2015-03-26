c22ac2b61fd55614c9d7a9e0b2483e6cc5f26e72
Damon Oehlman <damon.oehlman@gmail.com>
2015-03-27 09:09:34 +1100

    Create a new socket when creating a new signaller for the tests

--- test/all.js
+++ test/all.js
@@ -1,7 +1,12 @@
 var signaller = require('..');
 
 function createSignaller(opts) {
-  return signaller(location.origin, opts);
+  // create the new socket
+  // in our tests we forceNew to ensure that we don't multiplex on existing
+  // channels (which would just make things messy)
+  var socket = require('socket.io-client')(location.origin, { forceNew: true });
+
+  return signaller(socket, opts);
 }
 
 require('rtc-signaller-testrun')(createSignaller);
